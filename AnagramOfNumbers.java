f43c6851c8b26e8ca41bc01e4bf34e56f41742e4
Bikrant Jena <80874430+bikrant1332@users.noreply.github.com>
2023-04-16 13:54:47 +0530

    Add files via upload

---
+++ AnagramOfNumbers.java
@@ -0,0 +1,17 @@
+package hungers.practice.bj.miscelenious;
+
+public class AnagramOfNumbers {
+
+	public static void main(String[] args) {
+		int a = 695;
+		int b= 956;
+		boolean result = isAnagram(a, b);
+        System.out.println( (result) ? "Anagrams" : "Not Anagrams" );
+		
+	}
+
+	private static boolean isAnagram(int a, int b) {
+		return false;
+	}
+
+}
