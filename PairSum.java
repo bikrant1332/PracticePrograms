fff3bb6dd8c6b8a407064cdbdcf86527391ba4c9
Bikrant Jena <80874430+bikrant1332@users.noreply.github.com>
2023-04-19 13:08:31 +0530

    updated anot

--- PairSum.java
+++ PairSum.java
@@ -1,23 +1,58 @@
-package hungers.practice.bj.interviewquestions;
-
 public class PairSum {
 //travesring through ones
 	public static void main(String[] args) {
 		// sum 16?  -  { 1, 4, 45, 6, 10, 8 }
 		int sum=16;
-		int[] arr = { 1, 4, 45, 6, 10, 8 };
-		int low=0;
+		int[] arr = { 1, 4, 45,6, 6, 10, 8,9,7};
+
+		//aproach-1
+		//for this approach needed to sort the array
+		/*int low=0;
 		int high=arr.length-1;
-		int tempSum =0;
+		Arrays.sort(arr);		
 		while(low<high) {
-			tempSum=arr[low]+arr[high];
-			if(tempSum==sum) {
+		
+			if(arr[low]+arr[high]==sum) {
 				 System.out.println("The pair is : ("+ arr[low] + ", " + arr[high] + ")");
 			}
-			if(tempSum>sum) {
-				high--;
+			if((arr[low]+arr[high])>sum) {
+				high-=1;
+			} 
+			else {
+				low++;
 			}
-			low++;
+		}*/
+		
+		//aproach-2
+		//using hashmap
+		int rem=0;
+		int element=0;
+		int count=0;
+		Map<Integer, Integer> map= new HashMap<Integer, Integer>();
+		for(int i=0;i<arr.length;i++) {
+			element = arr[i];
+			rem=sum-element;
+			
+			//If array contains duplicate then this
+			if(map.containsKey(rem)) {
+				count = map.get(rem);
+				for(int j=0;j<count;j++) {
+					System.out.println("The pairs are : "+rem+","+element);
+				}
+			}
+
+			//if array not have any duplicates
+			/*if(map.containsKey(rem)) {
+				System.out.println("The pairs are : "+rem+","+element);
+			}*/
+			
+			if(map.containsKey(element)) {
+				map.put(element, map.get(element)+1);
+			}else {
+				map.put(element, 1);
+			}
+			
+			
 		}
 	}
 
