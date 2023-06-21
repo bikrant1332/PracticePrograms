b2d8a6ebcd173e1321df0bae3130d9bec404c394
Bikrant Jena <80874430+bikrant1332@users.noreply.github.com>
2023-06-21 18:31:39 +0530

    Create ReverseEachWordOfTheString.java

---
+++ ReverseEachWordOfTheString.java
@@ -0,0 +1,20 @@
+public class ReverseEachWordOfTheString {
+
+	public static void main(String[] args) {
+		//input = i.like.this.program.very.much
+		//output =i.ekil.siht.margorp.yrev.hcum
+		
+		//input = i want to go
+		//output = i tnaw ot og
+		
+		//input = quick brown fox jumps over lazy dog
+		String s = "quick brown fox jumps over lazy dog";
+
+		
+		String reverseWord = Arrays.stream(s.split(" "))
+				.map(word->new StringBuilder(word).reverse())
+				.collect(Collectors.joining(" "));
+		System.out.println("Output : "+reverseWord);
+	}
+
+}
